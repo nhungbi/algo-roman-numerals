5765f9d40935528c2adc344b098473bd854a18af
Joshua Alletto <joshalletto@gmail.com>
2018-06-21 10:16:08 -0500

    ruby 2 python

---
+++ python/roman_numerals_spec.py
@@ -0,0 +1,6 @@
+from roman_numerals import to_roman
+
+print(to_roman(1) == 'I')
+print(to_roman(3) == 'III')
+print(to_roman(4) == 'IV')
+# add tests to cover different edge cases
--- ruby/roman_numerals.rb
+++
@@ -1,3 +0,0 @@
-def to_roman(num) # hello i am messing with everything
-end
-123123123123123123123
