5414a932e6a00d5d51fb4fb0beb6d6e1e1376861
nhungbi <nhungvan16@gmail.com>
2022-05-24 10:17:03 -0500

    fn js

--- js/romanNumeralsSpec.js
+++ js/romanNumeralsSpec.js
@@ -3,3 +3,4 @@ var rn = require("./romanNumerals");
 console.log(rn.toRoman(1) === 'I');
 console.log(rn.toRoman(3) === 'III');
 console.log(rn.toRoman(4) === 'IV');
+console.log(rn.toRoman(14) === 'XIV');
